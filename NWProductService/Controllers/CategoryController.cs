1397f0ff2accab0325dcd0097473e83d2750d857
Nursen Özcan <nursenozcan95@gmail.com>
2023-01-16 19:54:53 +0300

    Add project files.

---
+++ NWProductService/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace NWProductService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+       
+    { 
+        [HttpGet(Name = "GetCategories")]
+        public IEnumerable <Models.Category> GetCat()
+        {
+            Models.NorthwindContext cnt = new Models.NorthwindContext();
+            return cnt.Categories;
+        }
+        
+    }
+}
---
+++ NWProductService/Models/UrunFiyatGecmisi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NWProductService.Models;
+
+public partial class UrunFiyatGecmisi
+{
+    public int Id { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public decimal? OldPrice { get; set; }
+
+    public decimal? NewPrice { get; set; }
+
+    public DateTime? DegistirmeTarihi { get; set; }
+}
